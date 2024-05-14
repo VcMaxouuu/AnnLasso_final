2c7213b42f53d28a63e29b304aa986e613af9f8c
maxime van cutsem <maxime.van-cutsem@etu.unige.ch>
2024-05-14 18:53:41 +0200

    class model update

--- models/AnnLassoClassification.py
+++ models/AnnLassoClassification.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import torch
 import torch.nn as nn
 import numpy as np
@@ -10,17 +9,4 @@ class AnnLassoClassification(ModelArchitecture):
         super.__init__(model_type = 1, p2=p2, lambda_qut=lambda_qut, device=device)
 
     def forward(self, X):
-=======
-import torch
-import torch.nn as nn
-import numpy as np
-import utils
-from .ModelArchitecture import ModelArchitecture
-
-class AnnLassoClassification(ModelArchitecture):
-    def __init__(self, p2=20, lambda_qut=None, device=None):
-        super.__init__(model_type = 1, p2=p2, lambda_qut=lambda_qut, device=device)
-
-    def forward(self, X):
->>>>>>> 0d8f64bfe45ee6b944a2e4379287e4d8ebf9d9d6
         pass
